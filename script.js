d04f7563a3f1beb6277020ac67cbf36bdf7d8c5f
Hrvoje <hrvoje.dakovic98@gmail.com>
2022-01-04 17:08:46 +0100

    Update

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
 const cells = document.querySelectorAll('[data-cell]')
 const hiddenMessage = document.querySelector('[data-winning-message]')
 const hiddenMessageElement = document.getElementById('hiddenMessage')
 const restartButton = document.getElementById('restartButton')
-let circleTurn
 
 const WINNER_COMBO = [
     [0,1,2],
@@ -17,62 +14,71 @@ const WINNER_COMBO = [
     [2,5,8]
 ]
 
-let handleClick = (e) => {
-    const cell = e.target
-    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-    placeMark(cell, currentClass)
-    if(checkWin(currentClass)) {
-        endGame(false)
-    } else if (isDraw()) {
-        endGame(true)
-    } else {
-    switchTurns()
-    }
-}
+const Game = () => {
+    const X_CLASS = 'x'
+    const CIRCLE_CLASS = 'circle'
+    let circleTurn
 
-let endGame = (draw) => {
-    if(draw) {
-        hiddenMessage.innerText = `It's a DRAW!`
-    } else {
-        hiddenMessage.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
-    }
-    hiddenMessageElement.classList.add('show')
-}
-
-let isDraw = () => {
-    return [...cells].every(cell => {
-        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-    })
-}
-
-let startGame = () => {
-    circleTurn = false
-    cells.forEach(cell => {
-        cell.classList.remove(X_CLASS)
-        cell.classList.remove(CIRCLE_CLASS)
-        cell.removeEventListener('click', handleClick)
-        cell.addEventListener('click', handleClick, { once:true })
-    })
-    hiddenMessageElement.classList.remove('show')
-}
-
-let placeMark = (cell, currentClass) => {
-    cell.classList.add(currentClass)
-}
-
-let switchTurns = () => {
-    circleTurn = !circleTurn
-}
-
-let checkWin = (currentClass) => {
-    return WINNER_COMBO.some(combination => {
-        return combination.every(index => {
-            return cells[index].classList.contains(currentClass)
+    const startGame = () => {
+        circleTurn = false
+        cells.forEach(cell => {
+            cell.classList.remove(X_CLASS)
+            cell.classList.remove(CIRCLE_CLASS)
+            cell.removeEventListener('click', handleClick)
+            cell.addEventListener('click', handleClick, { once:true })
         })
-    })
+        hiddenMessageElement.classList.remove('show')
+    }
+
+    const handleClick = (e) => {
+        const cell = e.target
+        const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+        placeMark(cell, currentClass)
+        if(checkWin(currentClass)) {
+            endGame(false)
+        } else if (isDraw()) {
+            endGame(true)
+        } else {
+        switchTurns()
+        }
+    }
+    
+    const endGame = (draw) => {
+        if(draw) {
+            hiddenMessage.innerText = `It's a DRAW!`
+        } else {
+            hiddenMessage.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+        }
+        hiddenMessageElement.classList.add('show')
+    }
+    
+    const isDraw = () => {
+        return [...cells].every(cell => {
+            return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+        })
+    }
+    
+    const placeMark = (cell, currentClass) => {
+        cell.classList.add(currentClass)
+    }
+    
+    const switchTurns = () => {
+        circleTurn = !circleTurn
+    }
+    
+    const checkWin = (currentClass) => {
+        return WINNER_COMBO.some(combination => {
+            return combination.every(index => {
+                return cells[index].classList.contains(currentClass)
+            })
+        })
+    }    
+    return {startGame, handleClick, endGame, isDraw, placeMark, switchTurns, checkWin}
 }
 
-restartButton.addEventListener('click', startGame)
-
-
-startGame()
+const newGame = Game()
+newGame.startGame()
+restartButton.addEventListener('click', () => {
+    const restartGame = Game()
+    restartGame.startGame()
+})
